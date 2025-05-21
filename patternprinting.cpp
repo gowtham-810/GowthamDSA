4129349ea44723be738b925c7570a00c756c1a00
gowtham-810 <152418854+gowtham-810@users.noreply.github.com>
2025-05-21 17:44:25 +0530

    Update patternprinting.cpp

--- patternprinting.cpp
+++ patternprinting.cpp
@@ -85,3 +85,60 @@ int main() {
   * * * * * 
     * * * 
       * 
+
+
+
+//PATTERN 14
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=5;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<i<<" "; 
+            
+        }
+        
+        cout<<endl;
+        
+    }
+   
+
+    return 0;
+}
+//or
+#include <iostream>
+using namespace std;
+
+
+int main() {
+     int n=5;
+     
+     
+     int ctr=1;
+     
+     
+     for(int i=0;i<n;i++){
+         for(int j=0;j<=i;j++){
+             cout<<ctr<<" ";
+             
+         }
+         ctr+=1;
+         cout<<endl;
+         
+         
+     }
+     
+
+    return 0;
+}
+//OUTPUT
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5 
+
+
